93c75ac433d4f863731cff20ea4d51ec40a191a7
Paulo Rnb <paulornb99@gmail.com>
2025-07-08 21:20:27 -0300

    Atualização do site

--- src/components/Drawer/DrawerHeader.tsx
+++ src/components/Drawer/DrawerHeader.tsx
@@ -13,11 +13,13 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import { Separator } from "@radix-ui/react-separator";
+import { useState } from "react";
 
 export function DrawerHeader() {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
-      <Dialog.Root>
+      <Dialog.Root open={open} onOpenChange={setOpen}>
         <Dialog.Trigger asChild>
           <button
             aria-label="Abrir o menu"
@@ -28,8 +30,18 @@ export function DrawerHeader() {
           </button>
         </Dialog.Trigger>
         <Dialog.Portal>
-          <Dialog.Overlay className="z-50 fixed inset-0 backdrop-blur-[2px] data-[state=open]:animate-fadeIn data-[state=closed]:animate-fadeOut" />
-          <Dialog.Content className="z-50 fixed top-0 right-0 h-full inset-y-0 w-full sm:max-w-sm md:max-w-[615px] border-l overflow-y-auto dark:bg-zinc-800 bg-white shadow-lg select-none flex flex-col data-[state=open]:animate-slideInRight data-[state=closed]:animate-slideOutRight">
+          <Dialog.Overlay
+            forceMount
+            className={`z-50 fixed inset-0 backdrop-blur-[2px] transition-opacity duration-300 ${
+              open ? "animate-fadeIn" : "animate-fadeOut"
+            }`}
+          />
+          <Dialog.Content
+            forceMount
+            className={`z-50 fixed top-0 right-0 h-full inset-y-0 border-l sm:max-w-sm md:max-w-[615px] w-full overflow-y-auto dark:bg-zinc-800 bg-white shadow-lg select-none flex flex-col transition-transform duration-300 ${
+              open ? "animate-slideInRight" : "animate-slideOutRight"
+            }`}
+          >
             <Dialog.Title asChild>
               <h2 className="text-lg font-semibold sticky top-0 z-[2] h-16 w-full bg-blue-700 dark:bg-zinc-900 md:flex md:items-center">
                 <Dialog.Close asChild>
